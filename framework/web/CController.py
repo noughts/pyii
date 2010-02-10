d52de24ab4a66fa19def03d0de545541bc3dcb62
U-noughts-MBP-win\noughts <noughts@noughts-MBP-win.(none)>
2010-02-11 06:55:36 +0900

    a

--- framework/web/CController.py
+++ framework/web/CController.py
@@ -1,13 +1,27 @@
-﻿from pyii.framework.web.CBaseController import CBaseController
+﻿import os
+import sys
+from google.appengine.ext.webapp import template
+from pyii.framework.web.CBaseController import CBaseController
 
 
 class CController( CBaseController ):
-
+	
+	# view ファイルのパスを返す
+	def getViewFile( self, viewName ):
+		path = os.path.abspath( "../protected/views/" + viewName );
+		return path;
+		
+	
+	def renderPartial( self, view, data=None ):
+		viewFile = self.getViewFile( view );
+		print template.render( viewFile, data )
+	
+	
 	def render( self, view, data=None ):
-		print "self"
+		print "self";
 	
 	
 	
 	
 	def renderText( self, text ):
-		print text
+		print text;
