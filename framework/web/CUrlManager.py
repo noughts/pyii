75a0ffdcc7a99162e9739c34789b70f0883629ae
noughts <noughts@gmail.com>
2010-03-03 16:04:15 +0900

    a

--- framework/web/CUrlManager.py
+++ framework/web/CUrlManager.py
@@ -1,11 +1,15 @@
 ﻿from pyii.framework.base.CApplicationComponent import CApplicationComponent;
+import cgi;
+import os;
 
 class CUrlManager( CApplicationComponent ):
 	
-	GET_FORMAT  ='get';
+	GET_FORMAT = 'get';
+	PATH_FORMAT = "path";
 	
 	def init( self ):
 		self._urlFormat = CUrlManager.GET_FORMAT;
+		self.caseSensitive = None;
 		#
 		CApplicationComponent.__init__( self );
 		self.processRules();
@@ -17,8 +21,19 @@ class CUrlManager( CApplicationComponent ):
 	
 	
 	def parseUrl( self, request ):
-		pass;
-	
+		if( self.getUrlFormat()==CUrlManager.PATH_FORMAT  ):
+			# ontheway
+			pass;
+		
+#		form = cgi.FieldStorage();
+#		form.getvalue("hogehoge")
+#		print( os.environ["PATH_INFO"] );
+#		print( os.environ["QUERY_STRING"] );
+		if( os.environ["QUERY_STRING"] == "" ):
+			return os.environ["PATH_INFO"];
+		else:
+			return os.environ["PATH_INFO"] +"?" + os.environ["QUERY_STRING"];
+
 	
 	
 	
--- framework/web/CWebApplication.py
+++ framework/web/CWebApplication.py
@@ -1,4 +1,5 @@
 ﻿from pyii.framework.base.CApplication import CApplication;
+from pyii.Pyii import Pyii;
 
 class CWebApplication( CApplication ):
 	
@@ -6,14 +7,53 @@ class CWebApplication( CApplication ):
 	
 	
 	def __init__( self ):
-		CApplication.__init__( self );
+		self.defaultController = "site";
 		self.catchAllRequest = None;
+		self.
+		
+		CApplication.__init__( self );
 	
 	
 	def processRequest( self ):
-		if type(self.catchAllRequest)=="list" and 0 in self.catchAllRequest:
+		if( (type(self.catchAllRequest)=="list") and (0 in self.catchAllRequest) ):
 			route = self.catchAllRequest[0];
 			#TODO
 		else:
 			route = self.getUrlManager().parseUrl( self.getRequest() );
 		self.runController( route );
+	
+	
+	def runController( self, route ):
+		self.createController( route );
+
+
+
+
+
+	def createController( self, route, owner=None ):
+		if( owner is None ):
+			owner = self;
+		
+		route = route.strip( "/" );
+		if( route=="" ):
+			route = self.defaultController;
+		
+		caseSensitive = self.getUrlManager().caseSensitive;		
+		route += "/";
+		while( route.find("/") >= 0 ):
+			pos = route.find( "/" );
+			id = route[0:pos];
+			route = route[pos+1:len(route)];
+			if( basePath is None ):
+				if( id in owner.controllerMap	 ):
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
